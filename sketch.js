7a7e2d316811283a3f5dd4ecad98674bc25bd1f9
krishna-mahesh-lad <68733354+krishna-mahesh-lad@users.noreply.github.com>
2020-10-01 18:02:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var gameState = 0;
+var playerCount, database; 
+var form, player, game;
+var allPlayers;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400, 400);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  if (playerCount === 4) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    clear();
+    game.play();
+  }
+    drawSprites();
+  
+}
+
